8b4e38db10dcdb9e9fe64cd418e4a9033b73b603
Gamal Mouhssine <95505686+gamalmouhssine@users.noreply.github.com>
2024-12-10 17:49:56 +0100

    Time: 4 ms (49.54%), Space: 46.9 MB (30.57%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cs
+++ 0012-integer-to-roman/0012-integer-to-roman.cs
@@ -1,14 +1,15 @@
 public class Solution {
     public string IntToRoman(int num) {
-        var roman = new StringBuilder();
+       
         int[] Value = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         string[] Symbol = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-        for(int i=0;i<Value.Length;i++){
-            while(num>=Value[i]){
-                 roman.Append(Symbol[i]);
-                 num -= Value[i];
-            }
+        string roman = ""; 
+    for (int i = 0; i < Value.Length && num > 0; i++) { 
+        while (num >= Value[i]) {
+            roman += Symbol[i];
+            num -= Value[i];
         }
-        return roman.ToString();
+    }
+    return roman;
     }
 }

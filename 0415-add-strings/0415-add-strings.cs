e54fa1456d18a664883090ead06b53c84fdb69d3
Gamal Mouhssine <95505686+gamalmouhssine@users.noreply.github.com>
2024-12-11 15:04:57 +0100

    Time: 8 ms (71.1%), Space: 43.5 MB (29.27%) - LeetHub

--- 0415-add-strings/0415-add-strings.cs
+++ 0415-add-strings/0415-add-strings.cs
@@ -1,21 +1,10 @@
 public class Solution {
     public string AddStrings(string num1, string num2) {
-        // var sum=Convert.ToInt32(num1)+Convert.ToInt32(num2);
-        //  return sum.ToString(); 
-        int i = num1.Length - 1;
-        int j = num2.Length - 1;
-        int carry = 0;
-        var result = new StringBuilder();
-        while (i >= 0 || j >= 0 || carry > 0)
-        {
-            int digit1 = i >= 0 ? num1[i] - '0' : 0;
-            int digit2 = j >= 0 ? num2[j] - '0' : 0;
-            int sum = digit1 + digit2 + carry;
-            result.Append(sum % 10);
-            carry = sum / 10;
-            i--;
-            j--;
-        }
-        return new string(result.ToString().Reverse().ToArray());
+       // var sum=Convert.ToInt32(num1)+Convert.ToInt32(num2);
+       //  return sum.ToString(); 
+       BigInteger n1 = BigInteger.Parse(num1);
+       BigInteger n2 = BigInteger.Parse(num2);
+       BigInteger sum = n1 + n2;
+       return sum.ToString();
     }
 }

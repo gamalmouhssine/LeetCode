27965ab6d3866d4bd955edfcf5afbedb9415a265
Gamal Mouhssine <95505686+gamalmouhssine@users.noreply.github.com>
2024-12-11 14:55:56 +0100

    Time: 1 ms (11.61%), Space: 64.3 MB (90.43%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public void ReverseString(char[] s) {
+        int n = s.Length; 
+        for (int i = 0; i < n / 2; i++) 
+        {
+            char temp = s[i];          
+            s[i] = s[n - i - 1];       
+            s[n - i - 1] = temp;       
+        }
+        
+    }
+}
